ff57a39f5b117f05b769975ffa937fdf7be7ba87
lumigam <lmgmwww@gmail.com>
2019-11-16 16:30:41 +0100

    Se agrega el shortcode para mostrar el formulario

---
+++ lmg-plugin-form.php
@@ -0,0 +1,14 @@
+<?php
+/*
+* Plugin Name: lmg-plugin-form
+* Author: Luismi G
+* Description: Plugin que genera un formulario utilizando el shortcode [lmg-plugin-form]
+*/
+
+add_shortcode( 'lmg-plugin-form', 'LMG-plugin-form' );
+
+function LMG-plugin-form{
+
+
+
+}
